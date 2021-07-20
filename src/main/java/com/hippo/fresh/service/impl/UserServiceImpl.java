a736e7eca0a814fe7cfaa5afd7e2c907c8aaf854
wyyadd <wyyadd@gmail.com>
2021-07-20 13:36:20 +0800

    [1.1.5]add ik_smart pinyin

--- src/main/java/com/hippo/fresh/service/impl/UserServiceImpl.java
+++ src/main/java/com/hippo/fresh/service/impl/UserServiceImpl.java
@@ -3,8 +3,10 @@ package com.hippo.fresh.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.hippo.fresh.dao.ReceiverRepository;
 import com.hippo.fresh.dao.UserRepository;
+import com.hippo.fresh.dao.VerificationRepository;
 import com.hippo.fresh.entity.Receiver;
 import com.hippo.fresh.entity.User;
+import com.hippo.fresh.entity.Verification;
 import com.hippo.fresh.exception.UserHasExistException;
 import com.hippo.fresh.exception.UserNotExistException;
 import com.hippo.fresh.service.UserService;
@@ -13,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +31,9 @@ public class UserServiceImpl implements UserService {
 
     @Autowired
     private ReceiverRepository receiverRepository;
+
+    @Autowired
+    private VerificationRepository verificationRepository;
 
     private JSONObject jsonObject;
     @Override
@@ -58,28 +65,61 @@ public class UserServiceImpl implements UserService {
         return user.isPresent();
     }
 
+
+    /** 判断验证码是否过期：5分钟 */
+    public boolean verCodeInvalid(Timestamp createTime, Timestamp nowTime) {
+        SimpleDateFormat timeformat = new SimpleDateFormat("yyyy-MM-dd,HH:mm:ss");
+        long t1 = createTime.getTime();
+        long t2 = nowTime.getTime();
+        int hours=(int) ((t2 - t1)/(1000*60*60));
+        int minutes=(int) (((t2 - t1)/1000-hours*(60*60))/60);
+//        int second=(int) ((t2 - t1)/1000-hours*(60*60)-minutes*60);
+        if(minutes >= 5)
+            return true;
+        else
+            return false;
+    }
+
     /** 用户注册
      * @return json
      * */
-    public ResponseUtils register(String username, String password, String email) {
+    public ResponseUtils register(String username, String password,String email,String verCode,Long verCodeId) {
 
-        Optional<User> user = userRepository.findByUsername(username);
+        Optional<Verification> verification = verificationRepository.findById(verCodeId);
 
-        boolean existsUser = user.isPresent();
-        //如果用户名不存在，成功创建用户
-        if (!existsUser) {
-//            System.out.println("true");
-            User newUser = userRepository.save(new User(username,bCryptPasswordEncoder.encode(password),email));
-            jsonObject = new JSONObject();
-            jsonObject.put("id", newUser.getId());
-            jsonObject.put("username", newUser.getUsername());
-            jsonObject.put("email", newUser.getEmail());
-            return ResponseUtils.response(200,"注册成功", jsonObject);
-        } else {
-            jsonObject = new JSONObject();
-            jsonObject.put("username",username);
-            jsonObject.put("email",email);
-            throw new UserHasExistException(jsonObject);
+        //用户没有点击获取验证码，前端传入的verCodeId = 0
+        if(!verification.isPresent()){
+            return ResponseUtils.response(401,"请获取验证码后再进行登录", jsonObject);
+        }
+        //用户已获取验证码
+        else{
+            if(verCodeInvalid(verification.get().getCreateTime(),new Timestamp(System.currentTimeMillis()))){
+                return ResponseUtils.response(402,"验证码过期，请重新获取验证码", jsonObject);
+            }
+            else{
+                //验证码错误
+                if(!verCode.equals(verification.get().getVerCode())){
+                    return ResponseUtils.response(403,"验证码错误，请重新输入验证码", jsonObject);
+                }
+                else{
+                    Optional<User> user = userRepository.findByUsername(username);
+
+                    boolean existsUser = user.isPresent();
+                    //用户名已存在
+                    if (existsUser) {
+                        jsonObject = new JSONObject();
+                        throw new UserHasExistException(jsonObject);
+                    }
+                    //验证码正确，用户名不存在，成功注册用户
+                    else {
+                        Timestamp createTime = new Timestamp(System.currentTimeMillis());
+                        User newUser = userRepository.save(new User(username,bCryptPasswordEncoder.encode(password),email,createTime));
+                        jsonObject = new JSONObject();
+                        return ResponseUtils.response(200,"注册成功", jsonObject);
+                    }
+                }
+
+            }
         }
     }
 
@@ -90,7 +130,7 @@ public class UserServiceImpl implements UserService {
         Optional<User> user = userRepository.findById(userId);
 
         if(user.isPresent()) {
-            List<Receiver> allReceiver = receiverRepository.findAllByUserId(userId);
+            List<Receiver> allReceiver = receiverRepository.findAllByUserIdOrderByUpdateTimeDesc(user.get().getId());
             jsonObject.put("id", user.get().getId());
             jsonObject.put("username", user.get().getUsername());
             jsonObject.put("email", user.get().getEmail());
@@ -102,6 +142,60 @@ public class UserServiceImpl implements UserService {
         {
             jsonObject.put("id",userId);
             throw new UserNotExistException(jsonObject);
+        }
+    }
+
+    /** 修改用户密码 */
+    public ResponseUtils passwordModify(Long userId,String oldPassword,String newPassword){
+
+        jsonObject = new JSONObject();
+
+        Optional<User> u = userRepository.findById(userId);
+        if(!u.isPresent()){
+            jsonObject.put("id",userId);
+            throw new UserNotExistException(jsonObject);
+        }
+        //用户存在
+        else {
+            User user = u.get();
+            String password = user.getPassword();
+            //用户输入原始密码与数据库保持一致
+            if(bCryptPasswordEncoder.matches(oldPassword,password)){
+                user.setPassword(bCryptPasswordEncoder.encode(newPassword));
+                userRepository.save(user);
+                return ResponseUtils.response(200,"用户密码修改成功", jsonObject);
+            }
+            else{
+                return ResponseUtils.response(404,"用户密码修改失败", jsonObject);
+            }
+        }
+    }
+
+    /** 修改用户个人信息*/
+    public ResponseUtils informationModify(Long userId,String username,String email,String phone){
+        jsonObject = new JSONObject();
+
+        Optional<User> u = userRepository.findById(userId);
+        if(!u.isPresent()){
+            jsonObject.put("id",userId);
+            throw new UserNotExistException(jsonObject);
+        }
+        //用户id存在
+        else {
+            User user = u.get();
+            Optional<User> existsUser = userRepository.findByUsername(username);
+            //新修改的用户名和他人相同
+            if(existsUser.isPresent()){
+                return ResponseUtils.response(404,"用户名已存在", jsonObject);
+            }
+            else{
+                user.setUsername(username);
+                user.setEmail(email);
+                user.setPhone(phone);
+                userRepository.save(user);
+                return ResponseUtils.response(200,"用户个人信息修改成功", jsonObject);
+            }
+
         }
     }
 
