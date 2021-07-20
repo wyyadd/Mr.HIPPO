1aaa7122376931fc19114972b19faa24bea42799
Yan <459155106@qq.com>
2021-07-20 17:11:27 +0800

    修改头像上传功能

--- src/main/java/com/hippo/fresh/fileUpLoad/UpLoadController.java
+++ src/main/java/com/hippo/fresh/fileUpLoad/UpLoadController.java
@@ -1,7 +1,12 @@
 package com.hippo.fresh.fileUpLoad;
 
 import com.alibaba.fastjson.JSONObject;
+import com.hippo.fresh.dao.UserRepository;
+import com.hippo.fresh.entity.User;
+import com.hippo.fresh.security.config.JWTConfig;
+import com.hippo.fresh.security.utils.JWTTokenUtil;
 import com.hippo.fresh.utils.ResponseUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,10 +17,15 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
 public class UpLoadController {
+
+
+    @Autowired
+    private UserRepository userRepository;
 
     //时间格式化
     private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd/");
@@ -36,6 +46,10 @@ public class UpLoadController {
     @PostMapping("/upload")
     public ResponseUtils upload(MultipartFile file, HttpServletRequest request){
         JSONObject jsonObject = new JSONObject();
+
+        //从token中获取id
+        String token = request.getHeader(JWTConfig.tokenHeader);
+        Long userId = JWTTokenUtil.parseAccessToken(token).getId();
 
         //1.文件保存地址的后半段目录：  2021/07/18/
         String directory = simpleDateFormat.format(new Date());
@@ -59,6 +73,11 @@ public class UpLoadController {
             //可访问url格式：  协议 :// ip地址 ：端口号 / 文件目录(/images/2020/03/15/xxx.jpg)
             String url = request.getScheme() + "://" + request.getServerName() + ":" + request.getServerPort() + "/upload/" + directory + newFileName;
             jsonObject.put("url",url);
+
+            User user = userRepository.findById(userId).get();
+            user.setAvatar(url);
+            userRepository.save(user);
+
             return ResponseUtils.response(200, "文件上传成功",jsonObject);
         } catch (IOException e) {
             e.printStackTrace();
