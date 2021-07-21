77097982ed6ace41551cc58db4c8e1e1c5a5d22f
Yan <459155106@qq.com>
2021-07-21 15:44:15 +0800

    支付宝

--- src/main/java/com/hippo/fresh/alipay/AlipayService.java
+++ src/main/java/com/hippo/fresh/alipay/AlipayService.java
@@ -28,7 +28,7 @@ public class AlipayService {
 
         System.out.println("到这里啦111");
         AlipayTradePagePayResponse pay = Factory.Payment.Page().pay("河马生鲜购物平台订单",orderId,
-                paymentMoney,"http://39.101.199.242/");
+                paymentMoney,"http://39.101.199.242/finalhome");
 
         System.out.println("到这里啦222");
         String payForm = null;
