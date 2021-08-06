ff3c2a4682aff4fcfc84fad46d66ec6bc8293c69
wyyadd <67038748+wyyadd@users.noreply.github.com>
2021-08-05 20:35:25 -0700

    Delete background directory

--- background/src/main/java/com/hippomanager/backend/service/IOrdService.java
+++
@@ -1,8 +0,0 @@
-package com.hippomanager.backend.service;
-
-import com.hippomanager.backend.Mapper.entity.Ord;
-import java.util.List;
-
-public interface IOrdService {
-    List<Ord> selectOrdAll();
-}
--- background/src/main/java/com/hippomanager/backend/service/IServerService.java
+++
@@ -1,9 +0,0 @@
-package com.hippomanager.backend.service;
-
-import com.hippomanager.backend.vo.ServerInformationVo;
-
-import java.util.List;
-
-public interface IServerService {
-    ServerInformationVo getServerInfo();
-}
