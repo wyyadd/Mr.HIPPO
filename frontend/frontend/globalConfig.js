c7b12dddce29e64b761deb722af00fc2acf7a081
wyyadd <67038748+wyyadd@users.noreply.github.com>
2021-08-05 20:34:37 -0700

    Delete frontend directory

--- frontend/frontend/globalConfig.js
+++
@@ -1,3 +0,0 @@
-globalConfig = { // 兼容ie
-    BASE_API: 'http://localhost:8181/', // 接口请求地址
-  }
--- frontend/frontend/src/views/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="about">
-    <h1>Mr. Hippo</h1>
-    <h1>暑期实训项目：河马先生</h1>
-  </div>
-</template>
